b3c0a35374306f61e9a327b5b6dc3ab447493675
Dudu <dudushr@yahoo.com>
2022-11-26 14:17:14 +0200

    Create dictionary

--- packages/english-practice/src/app/dictionary/dictionary/dictionary.component.ts
+++ packages/english-practice/src/app/dictionary/dictionary/dictionary.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EnglishWordComponent } from '../english-word/english-word.component';
 
 @Component({
   selector: 'myorg-dictionary',
@@ -6,7 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dictionary.component.scss'],
 })
 export class DictionaryComponent implements OnInit {
+  dictionary: Array<EnglishWordComponent> = [];
+
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+  }
+
+  public addWord(word: EnglishWordComponent){
+    if(!this.isWordExistInDictionary(word)){
+      this.dictionary.push(word);
+    }
+  }
+
+
+  public isWordExistInDictionary(word: EnglishWordComponent): boolean{
+   for(let i=0; i<this.dictionary.length; i++){
+      if(this.dictionary[i].englishWord === word.englishWord){
+       return true;
+      }
+    }
+
+    return false;
+  }
 }

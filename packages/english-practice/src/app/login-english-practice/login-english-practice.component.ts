d901a6ebde017492fc832c7c01e2873105858939
Dudu <dudushr@yahoo.com>
2023-05-26 18:00:32 +0300

    Login style

--- packages/english-practice/src/app/login-english-practice/login-english-practice.component.ts
+++ packages/english-practice/src/app/login-english-practice/login-english-practice.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { LoginManagerService } from '../services/login-manager.service';
+import { EnglishConfigurationService } from '../services/english-configuration.service';
 
 @Component({
   selector: 'myorg-login-english-practice',
@@ -11,11 +12,16 @@ export class LoginEnglishPracticeComponent implements OnInit {
   usersFormControl = new FormControl('');
   usersList: string[] = ['Elad', 'Dudu'];
 
-  constructor(private loginService: LoginManagerService) {}
+  constructor(private loginService: LoginManagerService, 
+              private configurationService: EnglishConfigurationService) {}
 
   ngOnInit(): void {}
 
   userSelected($event: any){
     this.loginService.setUser($event.value);
+  }
+
+  getClassName(className: string){
+    return this.configurationService.getClassName(className);
   }
 }
